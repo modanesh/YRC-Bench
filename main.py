720f18d3afd336872b5ace5067ef9f516cd51854
modanesh <mohamad4danesh@gmail.com>
2024-07-09 14:21:53 -0400

    adding cliport

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weak_model_file', type=str, required=True)
+    parser.add_argument('--weak_model_file', type=str)
     parser.add_argument('--strong_model_file', type=str)
     parser.add_argument('--help_policy_type', type=str, choices=['T1', 'T2', 'T3'], required=True,
                         help='Type of the helper policy. T1: vanilla PPO, T2: PPO with inputs concatenated by the weak agent features (conv + mlp), '
@@ -17,9 +17,10 @@ def get_args():
 
 if __name__ == '__main__':
     args = get_args()
+    env_registry.set_cfgs(name=args.benchmark)
 
     # get configs
-    exp_cfg = env_registry.get_cfgs(args)
+    exp_cfg = env_registry.setup_cfgs(args)
     environment = Environment(exp_cfg)
     policy = Policy(exp_cfg)
 
@@ -30,13 +31,19 @@ if __name__ == '__main__':
     obs_shape, action_size = environment.get_env_configs()
 
     # load executing policies
-    weak_policy, strong_policy = policy.load_acting_policies(obs_shape, action_size)
+    policy_kwargs = {'obs_shape': obs_shape, 'action_size': action_size}
+    weak_policy, strong_policy = policy.load_acting_policies(policy_kwargs)
 
-    # set up help environment
-    env, env_val = environment.make(weak_policy, strong_policy)
+    # Set up environment and additional variables based on benchmark
+    if args.benchmark == 'procgen':
+        env, env_val = environment.make(weak_policy, strong_policy)
+        additional_var = env_val
+    elif args.benchmark == 'cliport':
+        env, task = environment.make(weak_policy, strong_policy)
+        additional_var = task
 
     # set up help policy
-    help_algorithm = policy.setup_help_policy(env, env_val, weak_policy, strong_policy, logger, obs_shape)
+    help_algorithm = policy.setup_help_policy(env, additional_var, weak_policy, strong_policy, logger, obs_shape)
 
     # train the help policy
     help_algorithm.train(exp_cfg.num_timesteps)
