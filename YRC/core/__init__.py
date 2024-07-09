b39284eca4bb94902aabb415fe2fae3e11ed8b54
modanesh <mohamad4danesh@gmail.com>
2024-07-09 14:18:56 -0400

    adding cliport

--- YRC/core/__init__.py
+++ YRC/core/__init__.py
@@ -3,11 +3,12 @@
 from .environment import Environment
 from .policy import Policy
 from .utils import Logger
-# from .evaluator import Evaluator
 from .env_registry import env_registry
 from .procgen_config import ProcgenCfg
+from .cliport_config import CliportCfg
 
 
 __all__ = ['Environment', 'Policy', 'Logger']
+env_registry.register("procgen", ProcgenCfg())
+env_registry.register("cliport", CliportCfg())
 
-env_registry.register(ProcgenCfg())
