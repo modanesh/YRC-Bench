0fb23f1d9a0864fdf19600ab44b8f37a8fa5ad55
modanesh <mohamad4danesh@gmail.com>
2024-07-25 16:17:41 -0400

    adding test

---
+++ test.py
@@ -0,0 +1,33 @@
+from YRC.core import Environment, Policy
+from YRC.core import env_registry
+from YRC.core.utils import logger_setup, get_args
+
+if __name__ == '__main__':
+    args = get_args()
+    env_registry.set_cfgs(name=args.benchmark)
+
+    # get configs
+    exp_cfg = env_registry.setup_cfgs(args)
+    environment = Environment(exp_cfg)
+    policy = Policy(exp_cfg)
+
+    # setup logger
+    logger = logger_setup(exp_cfg, is_test=True)
+
+    # get env configs
+    obs_shape, action_size = environment.get_env_configs()
+
+    # load executing policies
+    policy_kwargs = {'obs_shape': obs_shape, 'action_size': action_size}
+    weak_policy, strong_policy = policy.load_acting_policies(policy_kwargs)
+
+    # Set up environment and additional variables based on benchmark. For procgen, the `additional_var` is the
+    # validation env, and for cliport, the `additional_var` is the task.
+    env, _, task = environment.make(weak_policy, strong_policy)
+
+    # set up help policy
+    help_algorithm = policy.setup_help_policy(env, None, task, weak_policy, logger, obs_shape)
+
+    # train the help policy
+    help_algorithm.test(exp_cfg.num_test_steps)
+    
