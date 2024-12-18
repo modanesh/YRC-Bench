467debe287010c3c2353d43408910b2022f0f4d4
modanesh <mohamad4danesh@gmail.com>
2024-12-18 08:43:39 -0500

    minor update

--- YRC/models/utils.py
+++ YRC/models/utils.py
@@ -100,3 +100,12 @@ class ResidualBlock(nn.Module):
 class Flatten(nn.Module):
     def forward(self, x):
         return torch.flatten(x, start_dim=1)
+
+
+def init_params(m):
+    classname = m.__class__.__name__
+    if classname.find("Linear") != -1:
+        m.weight.data.normal_(0, 1)
+        m.weight.data *= 1 / torch.sqrt(m.weight.data.pow(2).sum(1, keepdim=True))
+        if m.bias is not None:
+            m.bias.data.fill_(0)
