6c70278b35a163421e999e0a5db8c75ba9766a12
modanesh <mohamad4danesh@gmail.com>
2024-12-26 15:01:16 -0500

    minigrid updates

--- YRC/envs/minigrid/policies.py
+++ YRC/envs/minigrid/policies.py
@@ -1,15 +1,20 @@
 from YRC.core.policy import Policy
+import torch
+from YRC.core.configs import get_global_variable
 
 
 class MinigridPolicy(Policy):
-    def __init__(self, model):
+    def __init__(self, model, num_envs):
         self.model = model
+        self.memory = torch.zeros(num_envs, self.model.memory_size, device=get_global_variable("device"))
 
     def forward(self, obs):
+        obs = self.model.preprocess_obs(obs)
         return self.model.get_logit(obs)
 
     def predict(self, obs):
-        dist, value = self.model(obs)
+        obs = self.model.preprocess_obs(obs)
+        dist, value, self.memory = self.model(obs, self.memory)
         return dist
 
     def act(self, obs, greedy=False):
@@ -21,6 +26,7 @@ class MinigridPolicy(Policy):
         return action.cpu().numpy()
 
     def get_hidden(self, obs):
+        obs = self.model.preprocess_obs(obs)
         return self.model.get_hidden(obs)
 
     @property
--- YRC/envs/minigrid/wrappers.py
+++ YRC/envs/minigrid/wrappers.py
@@ -1,13 +1,22 @@
 import gymnasium as gym
-import random
+from gymnasium.vector.vector_env import VectorEnv
 
 
-class HardResetWrapper(gym.Wrapper):
-    def __init__(self, env, seed):
-        super().__init__(env)
-        self.seed = seed
+class HardResetWrapper(VectorEnv):
+    def __init__(self, env):
+        self.env = env
+        self.observation_space = env.observation_space
+        self.action_space = env.action_space
+        assert env.action_space.nvec.min() == env.action_space.nvec.max(), "Action space must be discrete"
+        self.action_space.n = env.action_space.nvec.min()
+        self.num_envs = env.num_envs
 
     def reset(self, **kwargs):
-        new_seed = random.randint(self.seed - 1000, self.seed + 1000)
-        obs, _ = self.env.reset(seed=new_seed, **kwargs)
+        obs, _ = self.env.reset(seed=self.env.np_random_seed[-1] + 1)
         return obs
+
+    def step(self, actions):
+        obs, reward, termination, truncation, info = self.env.step(actions)
+        done = termination | truncation  # wrapper for gymnasium to older gym
+        info = [{"env_reward": r} for r in reward]
+        return obs, reward, done, info
