ca9c991238f36413aee72ffbd7385000d6e3ba28
modanesh <mohamad4danesh@gmail.com>
2025-01-09 11:36:44 -0500

    minor bug fixed

--- YRC/core/configs/utils.py
+++ YRC/core/configs/utils.py
@@ -78,7 +78,7 @@ def load(yaml_file_or_str, flags=None):
 
     if config.eval_mode:
         if config.file_name is None:
-            log_file = os.path.join(config.experiment_dir, "eval.log")
+            log_file = os.path.join(config.experiment_dir, f"eval_seed_{seed}.log")
         elif config.file_name.__contains__("sim"):
             log_file = os.path.join(config.experiment_dir, f"eval_sim_seed_{seed}.log")
         elif config.file_name.__contains__("true"):
