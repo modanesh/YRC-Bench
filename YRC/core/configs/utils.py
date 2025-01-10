5445d97f7e97ca7070bde9bedb1f1b03e7ff89ef
modanesh <mohamad4danesh@gmail.com>
2025-01-10 16:18:05 -0500

    minor bug fixed

--- YRC/core/configs/utils.py
+++ YRC/core/configs/utils.py
@@ -85,7 +85,7 @@ def load(yaml_file_or_str, flags=None):
             log_file = os.path.join(config.experiment_dir, f"eval_true_seed_{seed}.log")
     else:
         log_file = os.path.join(config.experiment_dir, "run.log")
-
+    assert not os.path.exists(log_file), f"Log file {log_file} already exists!"
     set_global_variable("log_file", log_file)
 
     if os.path.isfile(log_file):
